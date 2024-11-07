842afbf8ec88a63e2e4698622b62179c06c68764
Hivna <hivna12@gmail.com>
2024-11-07 10:26:13 -0300

    feat: add status, priority, due date, and category selection to task creation form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_login
+  before_action :set_task, only: [:edit, :update, :destroy]
 
   def index
     @tasks = Task.where(user_id: current_user.id)  
@@ -8,6 +9,7 @@ class TasksController < ApplicationController
   
   def new
     @task = Task.new
+    @categories = Category.all
   end
   
   def create
@@ -17,20 +19,32 @@ class TasksController < ApplicationController
     if @task.save
       redirect_to tasks_path, notice: 'Tarefa criada com sucesso!'
     else
-      render :index
+      @categories = Category.all
+      render :new
     end
   end
 
   def edit
+    @categories = Category.all
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'Tarefa atualizada com sucesso!'
+    else
+      @categories = Category.all
+      render :edit
+    end
   end
 
   def destroy
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :description, :status, :priority, :due_date, :category_id) 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :user
-    belongs_to :category 
+    belongs_to :category , optional: true
 
     enum status: { pending: 'pending', in_progress: 'in_progress', completed: 'completed' }
+    enum priority: { urgent: 'urgent', high: 'high', medium: 'medium', low: 'low' }
 end
