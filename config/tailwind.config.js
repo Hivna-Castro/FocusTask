c60d24e0e03607ff1c80037122a0d0c06c8617ca
Hivna <hivna12@gmail.com>
2024-12-04 10:53:50 -0300

    feat: migrate styles to TailwindCSS and configure project setup

---
+++ config/tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#003566",
+        primary_hover: "#001f3f",
+        secondary: "#800020",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
